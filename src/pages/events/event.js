9132e4c29504a8f738245d64fac9ce6396ff7eac
Burak <14030652+BurakAy@users.noreply.github.com>
2023-03-09 12:13:19 -0600

    placeholder elements

--- src/pages/events/event.js
+++
@@ -1,5 +0,0 @@
-const Event = () => {
-  return <h1>Event</h1>;
-};
-
-export default Event;
--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,5 +1,27 @@
 const Events = () => {
-  return <h1>Events</h1>;
+  return (
+    <div>
+      <h1>Events</h1>
+      <div className="m-3">
+        <a href="">
+          <img />
+          <h2>Event</h2>
+        </a>
+      </div>
+      <div className="m-3">
+        <a href="">
+          <img />
+          <h2>Event</h2>
+        </a>
+      </div>
+      <div className="m-3">
+        <a href="">
+          <img />
+          <h2>Event</h2>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Events;
