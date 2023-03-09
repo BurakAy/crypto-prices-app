5d3da994bf6154749c93d7e3dca9d616cbef11f0
Burak <14030652+BurakAy@users.noreply.github.com>
2023-03-09 12:15:18 -0600

    created EventCategories component

---
+++ src/pages/events/[categories]/index.js
@@ -0,0 +1,27 @@
+const EventCategories = () => {
+  return (
+    <div>
+      <h1>Event Categories</h1>
+      <div className="m-3">
+        <a href="">
+          <img />
+          <h2>Event</h2>
+        </a>
+      </div>
+      <div className="m-3">
+        <a href="">
+          <img />
+          <h2>Event</h2>
+        </a>
+      </div>
+      <div className="m-3">
+        <a href="">
+          <img />
+          <h2>Event</h2>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default EventCategories;
