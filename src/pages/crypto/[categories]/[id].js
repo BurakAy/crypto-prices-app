a78bf79394214b3abcf59eaf1c00d9d9708d75b1
Burak <14030652+BurakAy@users.noreply.github.com>
2023-03-11 20:51:58 -0600

    getting individual coin data

--- src/pages/crypto/[categories]/[id].js
+++ src/pages/crypto/[categories]/[id].js
@@ -1,8 +1,32 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const CategoryCoin = ({ data }) => {
-  return <h1 className="text-center mt-3 mb-5">{data.id.toUpperCase()}</h1>;
+const CategoryCoin = ({
+  id,
+  image,
+  name,
+  ticker,
+  price,
+  rank,
+  high,
+  low,
+  percentChange,
+  sparkLine,
+}) => {
+  return (
+    <div className="flex justify-center items-center mt-3 mb-5">
+      <Image
+        className="mr-2"
+        src={image.small}
+        alt={name}
+        width="35"
+        height="35"
+      />
+      <h1>
+        {id.toUpperCase()} ({ticker})
+      </h1>
+    </div>
+  );
 };
 
 export default CategoryCoin;
@@ -10,11 +34,32 @@ export default CategoryCoin;
 export async function getServerSideProps(context) {
   const coinID = context?.params.id;
   const res = await fetch(
-    `https://api.coingecko.com/api/v3/coins/${coinID}?sparkline=true`
+    `https://api.coingecko.com/api/v3/coins/${coinID}?community_data=false&developer_data=false&sparkline=true`
   );
   const data = await res.json();
+  const id = data.id;
+  const image = data.image;
+  const name = data.name;
+  const ticker = data.symbol;
+  const price = data.market_data.current_price;
+  const rank = data.market_cap_rank;
+  const high = data.market_data.high_24h;
+  const low = data.market_data.low_24h;
+  const percentChange = data.market_data.price_change_percentage_24h;
+  const sparkLine = data.market_data.sparkline_7d;
 
   return {
-    props: { data },
+    props: {
+      id,
+      image,
+      name,
+      ticker,
+      price,
+      rank,
+      high,
+      low,
+      percentChange,
+      sparkLine,
+    },
   };
 }
