4bca945150253237c7a100686110d52ea36c8b8a
Burak <14030652+BurakAy@users.noreply.github.com>
2023-03-12 13:48:49 -0500

    added line chart to page

--- src/pages/crypto/[categories]/[id].js
+++ src/pages/crypto/[categories]/[id].js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link from "next/link";
+import LineChart from "@/components/LineChart";
 
 const CategoryCoin = ({
   id,
@@ -11,20 +11,24 @@ const CategoryCoin = ({
   high,
   low,
   percentChange,
-  sparkLine,
+  pdata,
 }) => {
   return (
-    <div className="flex justify-center items-center mt-3 mb-5">
-      <Image
-        className="mr-2"
-        src={image.small}
-        alt={name}
-        width="35"
-        height="35"
-      />
-      <h1>
-        {id.toUpperCase()} ({ticker})
-      </h1>
+    <div>
+      <div className="flex justify-center items-center mt-3 mb-5">
+        <Image
+          className="mr-2"
+          src={image.small}
+          alt={name}
+          width="35"
+          height="35"
+        />
+        <h1>
+          {id.toUpperCase()} ({ticker})
+        </h1>
+      </div>
+
+      <LineChart coinPrices={pdata} />
     </div>
   );
 };
@@ -41,12 +45,17 @@ export async function getServerSideProps(context) {
   const image = data.image;
   const name = data.name;
   const ticker = data.symbol;
+  const category = data.categories[1];
   const price = data.market_data.current_price;
   const rank = data.market_cap_rank;
   const high = data.market_data.high_24h;
   const low = data.market_data.low_24h;
   const percentChange = data.market_data.price_change_percentage_24h;
-  const sparkLine = data.market_data.sparkline_7d;
+
+  const pres = await fetch(
+    `https://api.coingecko.com/api/v3/coins/${coinID}/market_chart?vs_currency=usd&days=7`
+  );
+  const pdata = await pres.json();
 
   return {
     props: {
@@ -54,12 +63,13 @@ export async function getServerSideProps(context) {
       image,
       name,
       ticker,
+      category,
       price,
       rank,
       high,
       low,
       percentChange,
-      sparkLine,
+      pdata,
     },
   };
 }
