0bcc6a3a7893d8bf6ac9977f670df7fba533f9fb
Burak <14030652+BurakAy@users.noreply.github.com>
2023-03-15 14:21:01 -0500

    navbar styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,11 +2,17 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <header className="p-6">
-      <nav className="flex justify-around text-lg">
-        <Link href="/">Home</Link>
-        <Link href="/crypto">Crypto Categories</Link>
-        <Link href="/about">About</Link>
+    <header className="p-6 shadow bg-gradient-to-r from-emerald-50 via--emerald-100 to-emerald-200">
+      <nav className="flex justify-around text-lg font-medium">
+        <Link className="hover:text-stone-600" href="/">
+          Home
+        </Link>
+        <Link className="hover:text-stone-600" href="/crypto">
+          Crypto Categories
+        </Link>
+        <Link className="hover:text-stone-600" href="/about">
+          About
+        </Link>
       </nav>
     </header>
   );
