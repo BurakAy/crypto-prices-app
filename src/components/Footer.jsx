5cbe6be6e21759c7c279b61e54858fc89281b890
Burak <14030652+BurakAy@users.noreply.github.com>
2023-03-15 22:22:47 -0500

    added link to linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,21 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="mt-5 mb-3 text-center">
-      Burak Aydemir Development | Data provided by{" "}
+    <footer className="mt-5 mb-3 text-center text-sm">
       <Link
         className="text-blue-600"
+        href="https://www.linkedin.com/in/aydemirburak/"
+        target="_blank"
+      >
+        Burak Aydemir
+      </Link>{" "}
+      Development |&nbsp;
+      <Link
+        className="text-blue-600 text-xs"
         href="https://www.coingecko.com/en/api"
         target="_blank"
       >
-        CoinGecko API
+        Data provided by CoinGecko API
       </Link>
     </footer>
   );
